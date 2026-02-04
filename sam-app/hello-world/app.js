378c548d32c68115d69cdd93ebfe7c4fa7a4dc1f
Yuanzhe Li <sidkey.yuanzhe@gmail.com>
2026-02-03 23:04:30 -0800

    fix the deployment

--- sam-app/hello-world/app.js
+++ sam-app/hello-world/app.js
@@ -1,22 +1,10 @@
-import AWS from 'aws-sdk';
-
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
+const AWS = require('aws-sdk'); // Use CommonJS syntax for AWS SDK v2
 
 const s3 = new AWS.S3();
 
-export const lambdaHandler = async (event, context) => {
+exports.lambdaHandler = async (event, context) => {
   try {
+    console.log('Lambda function triggered!');
     console.log('Event:', JSON.stringify(event, null, 2));
 
     // Extract bucket name and object key from the event
@@ -36,7 +24,6 @@ export const lambdaHandler = async (event, context) => {
 
     console.log('File Content:', fileContent);
 
-    // Return a success response
     return {
       statusCode: 200,
       body: JSON.stringify({
